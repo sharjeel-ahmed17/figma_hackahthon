293a0ee7cd30ac0b57db066346fc5845c1c9ea70
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-12-07 23:19:08 +0500

    testing route for testing install shadcn ui

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Testing = () => {
+  return (
+    <div>
+
+        <Button className='bg-red-800 text-blue-300' size='lg'>sharjeel alibari</Button>
+    </div>
+  )
+}
+
+export default Testing
